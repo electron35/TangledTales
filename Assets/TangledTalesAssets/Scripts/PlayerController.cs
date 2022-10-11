7ef0ac93772bf0e0d102deee71bfa2ed3608360a
Vinicius Mattei <vinicius.mattei@etu.unice.fr>
2022-10-11 11:57:13 +0200

    Basic Movement of player done

--- Assets/TangledTalesAssets/Scripts/PlayerController.cs
+++ Assets/TangledTalesAssets/Scripts/PlayerController.cs
@@ -2,8 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
+
+    [Range(5, 15)]
+    public float maxSpeed = 10;
+    public Vector2 move;
+    public Vector2 targetVelocity;
+    public Rigidbody2D body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.isKinematic = true;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +27,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        move.x = Input.GetAxis("Horizontal");
+
+        ComputeVelocity();
+    }
+
+    void ComputeVelocity()
+    {
+        targetVelocity = move * maxSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        body.position += targetVelocity / 100;    
+    }
+
+    public enum JumpState
+    {
+        Grounded,
+        PrepareToJump,
+        Jumping,
+        InFlight,
+        Landed
     }
 }
